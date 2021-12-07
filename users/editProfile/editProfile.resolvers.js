135f2f7712c0011efdc3ac51672ff238ee3a551b
vangona <rlarhksrud12@naver.com>
2021-12-07 18:57:38 +0900

    2021.12.07 #4.10

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,22 +1,26 @@
 import client from "../../client";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export default {
     Mutation: {
-        editProfile: async (_, {
-            firstName,
+        editProfile: async (_, 
+            { firstName,
             lastName,
             username,
             email,
             password:newPassword,
-        }) => {
+            },
+            { loggedInUser }
+        ) => {
+            console.log(loggedInUser);
             let uglyPassword = null;
             if (newPassword) {
                 uglyPassword = await bcrypt.hash(newPassword, 10)
             }
             const updatedUser = await client.user.update({
                 where:{
-                    id: 1
+                    id: loggedInUser.id,
                 }, 
                     data:{
                     firstName,
