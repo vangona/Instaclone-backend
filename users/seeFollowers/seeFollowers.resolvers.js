c70a3a8d31f7dcaa7dbf65339914dc1af1499402
vangona <rlarhksrud12@naver.com>
2022-06-14 16:17:45 +0900

    #4.24 Followers Pagination part Two

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,6 +3,18 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({
@@ -10,9 +22,14 @@ export default {
           skip: (page - 1) * 5,
         });
 
+      const totalFollowers = await client.user.count({
+        where: { following: { some: { username } } },
+      });
+
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollowers / 5),
       };
     },
   },
